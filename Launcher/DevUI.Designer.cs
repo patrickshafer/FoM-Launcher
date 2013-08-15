9fff283301aa8fbc7b851777adc3dbd03f2aa3c7
Patrick Shafer <patrick.shafer@gmail.com>
2013-08-15 12:37:51 -0600

    Updated the Launcher DevUI with default values

--- Launcher/DevUI.Designer.cs
+++ Launcher/DevUI.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.gbApplyPatch = new System.Windows.Forms.GroupBox();
-            this.LocalFolderLabel = new System.Windows.Forms.Label();
-            this.ManifestURLLabel = new System.Windows.Forms.Label();
-            this.LocalFolder = new System.Windows.Forms.TextBox();
-            this.ManifestURL = new System.Windows.Forms.TextBox();
-            this.LocalFolderBrowse = new System.Windows.Forms.Button();
             this.InvokeButton = new System.Windows.Forms.Button();
+            this.LocalFolderBrowse = new System.Windows.Forms.Button();
+            this.ManifestURL = new System.Windows.Forms.TextBox();
+            this.LocalFolder = new System.Windows.Forms.TextBox();
+            this.ManifestURLLabel = new System.Windows.Forms.Label();
+            this.LocalFolderLabel = new System.Windows.Forms.Label();
             this.gbApplyPatch.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,14 +53,41 @@
             this.gbApplyPatch.TabStop = false;
             this.gbApplyPatch.Text = "ApplyPatch()";
             // 
-            // LocalFolderLabel
+            // InvokeButton
             // 
-            this.LocalFolderLabel.AutoSize = true;
-            this.LocalFolderLabel.Location = new System.Drawing.Point(6, 26);
-            this.LocalFolderLabel.Name = "LocalFolderLabel";
-            this.LocalFolderLabel.Size = new System.Drawing.Size(62, 13);
-            this.LocalFolderLabel.TabIndex = 0;
-            this.LocalFolderLabel.Text = "LocalFolder";
+            this.InvokeButton.Location = new System.Drawing.Point(295, 75);
+            this.InvokeButton.Name = "InvokeButton";
+            this.InvokeButton.Size = new System.Drawing.Size(75, 23);
+            this.InvokeButton.TabIndex = 3;
+            this.InvokeButton.Text = "Invoke";
+            this.InvokeButton.UseVisualStyleBackColor = true;
+            this.InvokeButton.Click += new System.EventHandler(this.InvokeButton_Click);
+            // 
+            // LocalFolderBrowse
+            // 
+            this.LocalFolderBrowse.Location = new System.Drawing.Point(376, 21);
+            this.LocalFolderBrowse.Name = "LocalFolderBrowse";
+            this.LocalFolderBrowse.Size = new System.Drawing.Size(26, 23);
+            this.LocalFolderBrowse.TabIndex = 1;
+            this.LocalFolderBrowse.Text = "...";
+            this.LocalFolderBrowse.UseVisualStyleBackColor = true;
+            this.LocalFolderBrowse.Click += new System.EventHandler(this.LocalFolderBrowse_Click);
+            // 
+            // ManifestURL
+            // 
+            this.ManifestURL.Location = new System.Drawing.Point(81, 49);
+            this.ManifestURL.Name = "ManifestURL";
+            this.ManifestURL.Size = new System.Drawing.Size(289, 20);
+            this.ManifestURL.TabIndex = 2;
+            this.ManifestURL.Text = "http://patch.patrickshafer.com/Test.xml";
+            // 
+            // LocalFolder
+            // 
+            this.LocalFolder.Location = new System.Drawing.Point(81, 23);
+            this.LocalFolder.Name = "LocalFolder";
+            this.LocalFolder.Size = new System.Drawing.Size(289, 20);
+            this.LocalFolder.TabIndex = 0;
+            this.LocalFolder.Text = "C:\\Code\\fom-launcher\\TestData\\LocalFolder";
             // 
             // ManifestURLLabel
             // 
@@ -71,42 +98,18 @@
             this.ManifestURLLabel.TabIndex = 1;
             this.ManifestURLLabel.Text = "ManifestURL";
             // 
-            // LocalFolder
+            // LocalFolderLabel
             // 
-            this.LocalFolder.Location = new System.Drawing.Point(81, 23);
-            this.LocalFolder.Name = "LocalFolder";
-            this.LocalFolder.Size = new System.Drawing.Size(289, 20);
-            this.LocalFolder.TabIndex = 2;
-            // 
-            // ManifestURL
-            // 
-            this.ManifestURL.Location = new System.Drawing.Point(81, 49);
-            this.ManifestURL.Name = "ManifestURL";
-            this.ManifestURL.Size = new System.Drawing.Size(289, 20);
-            this.ManifestURL.TabIndex = 4;
-            // 
-            // LocalFolderBrowse
-            // 
-            this.LocalFolderBrowse.Location = new System.Drawing.Point(376, 21);
-            this.LocalFolderBrowse.Name = "LocalFolderBrowse";
-            this.LocalFolderBrowse.Size = new System.Drawing.Size(26, 23);
-            this.LocalFolderBrowse.TabIndex = 3;
-            this.LocalFolderBrowse.Text = "...";
-            this.LocalFolderBrowse.UseVisualStyleBackColor = true;
-            this.LocalFolderBrowse.Click += new System.EventHandler(this.LocalFolderBrowse_Click);
-            // 
-            // InvokeButton
-            // 
-            this.InvokeButton.Location = new System.Drawing.Point(295, 75);
-            this.InvokeButton.Name = "InvokeButton";
-            this.InvokeButton.Size = new System.Drawing.Size(75, 23);
-            this.InvokeButton.TabIndex = 5;
-            this.InvokeButton.Text = "Invoke";
-            this.InvokeButton.UseVisualStyleBackColor = true;
-            this.InvokeButton.Click += new System.EventHandler(this.InvokeButton_Click);
+            this.LocalFolderLabel.AutoSize = true;
+            this.LocalFolderLabel.Location = new System.Drawing.Point(6, 26);
+            this.LocalFolderLabel.Name = "LocalFolderLabel";
+            this.LocalFolderLabel.Size = new System.Drawing.Size(62, 13);
+            this.LocalFolderLabel.TabIndex = 0;
+            this.LocalFolderLabel.Text = "LocalFolder";
             // 
             // DevUI
             // 
+            this.AcceptButton = this.InvokeButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(451, 148);
