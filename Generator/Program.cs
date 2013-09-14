752afcd3fb076f1aaf13d9d1765db076ff3011fc
Patrick Shafer <patrick.shafer@gmail.com>
2013-09-13 22:19:45 -0600

    Added self-update to the generator app

--- Generator/Program.cs
+++ Generator/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using System.Globalization;
 
 namespace FoM.Generator
 {
@@ -14,6 +16,40 @@ namespace FoM.Generator
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+            Startup thisStartup = new Startup();
+            thisStartup.Execute();
+        }
+
+        static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            AssemblyName assemblyName = new AssemblyName(args.Name);
+
+            string path = assemblyName.Name + ".dll";
+
+            if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false)
+                path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+
+            using (System.IO.Stream stream = executingAssembly.GetManifestResourceStream(path))
+            {
+                if (stream == null)
+                    return null;
+                byte[] assemblyRawBytes = new byte[stream.Length];
+                stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+                return Assembly.Load(assemblyRawBytes);
+            }
+        }
+    }
+    internal class Startup
+    {
+        internal void Execute()
+        {
+            //log4net.Config.XmlConfigurator.Configure(System.Reflection.Assembly.GetEntryAssembly().GetManifestResourceStream("FoM.Launcher.Resources.log4netConfig.xml"));
+
+            FoM.PatchLib.PatchManager.ApplicationStart("http://patch.patrickshafer.com/generator.xml");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new DevUI());
