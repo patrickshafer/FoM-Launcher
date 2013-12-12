9049c31a01e43be1594ae281e876ed898407d7aa
Patrick Shafer <patrick.shafer@gmail.com>
2013-12-12 09:59:13 -0700

    Modified DelegateCommand to support functions with arguments.

--- Launcher/ViewModels/DelegateCommand.cs
+++ Launcher/ViewModels/DelegateCommand.cs
@@ -6,7 +6,9 @@ namespace FoM.Launcher.ViewModels
     public class DelegateCommand : ICommand
     {
         private readonly Func<bool> _CanExecute;
+        private readonly Func<object, bool> _CanExecuteP1;
         private readonly Action _ActionExecute;
+        private readonly Action<object> _ActionExecuteP1;
 
         public event EventHandler CanExecuteChanged;
 
@@ -18,14 +20,27 @@ namespace FoM.Launcher.ViewModels
 
         public DelegateCommand(Action ActionExecute) : this(ActionExecute, () => true) { }
 
+        public DelegateCommand(Action<object> ActionExecute, Func<object, bool> CanExecute)
+        {
+            this._ActionExecuteP1 = ActionExecute;
+            this._CanExecuteP1 = CanExecute;
+        }
+
         public void Execute(object parameter)
         {
-            this._ActionExecute();
+            if (this._ActionExecute != null)
+                this._ActionExecute();
+            if (this._ActionExecuteP1 != null)
+                this._ActionExecuteP1(parameter);
         }
 
         public bool CanExecute(object parameter)
         {
-            return this._CanExecute == null || this._CanExecute();
+            if (this._CanExecute != null)
+                return this._CanExecute();
+            if (this._CanExecuteP1 != null)
+                return this._CanExecuteP1(parameter);
+            return true;
         }
 
         public void RaiseCanExecuteChanged()
