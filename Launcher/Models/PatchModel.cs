25caf6fa254f4d769da9f8d14b0e371c20a52f47
Patrick Shafer <patrick.shafer@gmail.com>
2014-02-06 07:28:37 -0700

    Fixed an issue where auto-launch settings weren't being read from the right location

--- Launcher/Models/PatchModel.cs
+++ Launcher/Models/PatchModel.cs
@@ -94,8 +94,7 @@ namespace FoM.Launcher.Models
 
             if (File.Exists(FoMClient))
             {
-                Preferences PrefData = Preferences.Load();
-                string CmdLine = String.Format("-rez Resources -dpsmagic 1 +windowed {0}", PrefData.WindowedMode.GetHashCode());
+                string CmdLine = String.Format("-rez Resources -dpsmagic 1 +windowed {0}", LauncherApp.Instance.PreferenceInfo.WindowedMode.GetHashCode());
                 System.Diagnostics.Process.Start("fom_client.exe", CmdLine);
                 Log.Debug(String.Format("Opening fom_client.exe with \"{0}\"", CmdLine));
                 LauncherApp.Instance.Exit();
@@ -108,8 +107,7 @@ namespace FoM.Launcher.Models
         }
         public void StartAutoLaunch()
         {
-            Preferences PrefData = Preferences.Load();
-            if (PrefData.AutoLaunch)
+            if (LauncherApp.Instance.PreferenceInfo.AutoLaunch)
             {
                 this._AutoLaunchTicker = 4;
                 this._AutoLaunchTimer = new System.Windows.Threading.DispatcherTimer(System.Windows.Threading.DispatcherPriority.Normal, LauncherApp.Instance.ThisApp.Dispatcher);
