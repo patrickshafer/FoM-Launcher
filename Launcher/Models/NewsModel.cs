ef97d5c9690ec8e84901b0f0a44c13177e09e763
Patrick Shafer <patrick.shafer@gmail.com>
2014-03-12 11:19:41 -0600

    Added stub Model class for News items

---
+++ Launcher/Models/NewsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+
+
+namespace FoM.Launcher.Models
+{
+    class NewsModel
+    {
+        private const string RSSURL = @"http://forums.faceofmankind.com/external.php?forumids=299&type=rss";
+    }
+}
