30b005258d5e15651140cd82b1fb8a5ad521108a
Patrick Shafer <patrick.shafer@gmail.com>
2014-03-20 15:18:03 -0600

    Added article URL to NewsModel

--- Launcher/Models/NewsModel.cs
+++ Launcher/Models/NewsModel.cs
@@ -27,14 +27,8 @@ namespace FoM.Launcher.Models
                         SummaryText = NewsFeedItem.Summary.Text;
                         if (SummaryText.Contains("\n"))
                             SummaryText = SummaryText.Remove(SummaryText.IndexOf("\n"));
-                        this._NewsItems.Add(new NewsItem() {Title = NewsFeedItem.Title.Text, Summary = SummaryText, PublishDate = NewsFeedItem.PublishDate.Date });
+                        this._NewsItems.Add(new NewsItem() {Title = NewsFeedItem.Title.Text, Summary = SummaryText, PublishDate = NewsFeedItem.PublishDate.Date, ArticleURL = NewsFeedItem.Id });
                     }
-
-                    /*
-                    this._NewsItems.Add(new NewsItem() { Title = "Title one", Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at condimentum massa. Sed ac auctor nunc. Vivamus dignissim est a eleifend semper. Integer facilisis mi a lorem auctor posuere. Phasellus malesuada in dolor sed ornare. Fusce at odio ac dui porttitor posuere. Ut eu tempor lorem.", PublishDate = DateTime.Now.AddDays(-5) });
-                    this._NewsItems.Add(new NewsItem() { Title = "Title 2", Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at condimentum massa. Sed ac auctor nunc. Vivamus dignissim est a eleifend semper. Integer facilisis mi a lorem auctor posuere. Phasellus malesuada in dolor sed ornare. Fusce at odio ac dui porttitor posuere. Ut eu tempor lorem.", PublishDate = DateTime.Now.AddDays(-4) });
-                    this._NewsItems.Add(new NewsItem() { Title = "Title three", Summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at condimentum massa. Sed ac auctor nunc. Vivamus dignissim est a eleifend semper. Integer facilisis mi a lorem auctor posuere. Phasellus malesuada in dolor sed ornare. Fusce at odio ac dui porttitor posuere. Ut eu tempor lorem.", PublishDate = DateTime.Now.AddDays(-1) });
-                    */
                 }
                 return this._NewsItems;
             }
@@ -45,5 +39,6 @@ namespace FoM.Launcher.Models
         public string Title { get; set; }
         public string Summary { get; set; }
         public DateTime PublishDate { get; set; }
+        public string ArticleURL { get; set; }
     }
 }
